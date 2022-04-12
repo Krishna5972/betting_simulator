ee7948b39141a1c53d3f8cd08876ea1c1c1e268b
unknown <vamsikrishnagannamaneni@gmail.com>
2022-04-12 15:54:15 +0530

    added login and register pages

--- simulator.py
+++ simulator.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 from os import environ
 
 app=Flask(__name__)
-
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 open_bet_ids=[1,4,21,99]
 
@@ -14,6 +16,27 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'krishna5972' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 def main():
